30eeccf06a904e6b24c5919715a99d6148e8cc1d
Andrew Reid East <andrew@andreweast.net>
2019-01-28 12:49:16 +0000

    Update interfaces (spelling, etc.

--- src/main/java/net/teamtrycatch/server/Account.java
+++ src/main/java/net/teamtrycatch/server/Account.java
@@ -1,4 +1,14 @@
 package net.teamtrycatch.server;
 
+import java.util.Date;
+import java.util.List;
+
 public interface Account {
+    public int getAccountNum();
+
+    public String getAccountName(); // returns name of account holder
+
+    public List getAllTransactions();
+
+    public List getTransactionRange(Date beginDate, Date endDate); // returns only those transactions within a date range (inclusive)
 }
--- src/main/java/net/teamtrycatch/shared/Transaction.java
+++ src/main/java/net/teamtrycatch/shared/Transaction.java
@@ -1,4 +1,12 @@
 package net.teamtrycatch.shared;
 
-public interface Transaction {
+import java.io.Serializable;
+import java.util.Date;
+
+public interface Transaction extends Serializable {
+    public String getDescription();
+
+    public Date getDate();
+
+    public int getAmount();
 }
