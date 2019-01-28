66263ccb6618b88024da262ad0e325362810c5fd
Andrew Reid East <andrew@andreweast.net>
2019-01-28 12:52:37 +0000

    Create a basic server runner, mainly from the Oracle RMI tutorial

--- src/main/java/net/teamtrycatch/server/Bank.java
+++ src/main/java/net/teamtrycatch/server/Bank.java
@@ -6,40 +6,90 @@ import net.teamtrycatch.shared.InvalidSession;
 import net.teamtrycatch.shared.Statement;
 
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class Bank extends UnicastRemoteObject implements BankInterface {
+public class Bank implements BankInterface {
+//DEBUG public class Bank extends UnicastRemoteObject implements BankInterface {
     private List<Account> accounts; // users accounts
 
     public Bank() throws RemoteException {
+        accounts = new ArrayList<>();
     }
 
     public long login(String username, String password) throws RemoteException, InvalidLogin {
-        //implementation code
+        System.out.println("login!");
+        // TODO: implementation code
         return -1;
     }
 
     public void deposit(int account, int amount, long sessionID) throws RemoteException, InvalidSession {
-        // implementation code
+        System.out.println("deposit!");
+        // TODO: implementation code
     }
 
     public void withdraw(int account, int amount, long sessionID) throws RemoteException, InvalidSession {
-        // implementation code
+        System.out.println("withdraw!");
+        // TODO: implementation code
     }
 
     public int inquiry(int account, long sessionID) throws RemoteException, InvalidSession {
-        // implementation code
+        System.out.println("inquery!");
+        // TODO: implementation code
         return -1;
     }
 
     public Statement getStatement(Date from, Date to, long sessionID) throws RemoteException, InvalidSession {
-        // implementation code
+        System.out.println("getStatement!");
+        // TODO: implementation code
         return null;
     }
 
-    public static void main(String[] args) throws Exception {
-        // initialise Bank server - see sample code in the notes and online RMI tutorials for details
+    /**
+     * Start the Bank server
+     * Much of this code is derived from the Oracle Java RMI Tutorial path
+     */
+    public static void main(String[] args) {
+        // Start a security manager, else RMI will not download classes
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+
+        try {
+            // Create bank instance (this class)
+            Bank bank = new Bank(); // DEBUG: Does this need to be the public, over-the-wire interface?
+//            BankInterface bank = new Bank();
+
+            bank.createMockAccounts();
+
+            // Create RMI server as a UnicastRemoteObject
+            int port;
+            if (args.length != 0) {
+                port = Integer.parseInt(args[0]);
+            } else {
+                // Port 0 means use the default
+                port = 0;
+            }
+            BankInterface stub = (BankInterface) UnicastRemoteObject.exportObject(bank, port);
+
+            // Bind compute engine to name server
+            Registry registry = LocateRegistry.getRegistry();
+            registry.rebind("Bank", stub);
+            System.out.println("Bank server has been launched and bound to port " + port);
+        } catch (RemoteException e) {
+            // Swallow exception
+            System.err.println("Bank Server RemoteException!");
+            e.printStackTrace();
+        }
+    }
+
+    private void createMockAccounts() {
+        PersonalAccount jack = new PersonalAccount(100, "Jack Doe");
+        jack.addTransaction(new InitialTransaction(new Date(), 1000));
+        // TODO: Continue here!
     }
 }
