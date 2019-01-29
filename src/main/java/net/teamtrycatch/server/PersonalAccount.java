24459eaf883f521719e8373a9b33c9695e85b671
Andrew Reid East <andrew@andreweast.net>
2019-01-29 15:21:14 +0000

    Update Account interface with more information, as needed for a login system

--- src/main/java/net/teamtrycatch/server/PersonalAccount.java
+++ src/main/java/net/teamtrycatch/server/PersonalAccount.java
@@ -2,23 +2,35 @@ package net.teamtrycatch.server;
 
 import net.teamtrycatch.shared.Transaction;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
 public class PersonalAccount implements Account {
     private int accountNum;
     private String accountHolderName;
+    private String username;
+    private byte[] passwordHashed;
+    private byte[] passwordSalt;
     private ArrayList<Transaction> transactions;
 
-    public PersonalAccount(int accountNum, String accountHolderName) {
+    public PersonalAccount(int accountNum, String accountHolderName, String username, String password) {
         this.accountNum = accountNum;
         this.accountHolderName = accountHolderName;
+        this.username = username;
+        this.saveHashedPassword(password);
         transactions = new ArrayList<>();
     }
 
-    public PersonalAccount(int accountNum, String accountHolderName, List<Transaction> importTransactions) {
-        this(accountNum, accountHolderName);
+    public PersonalAccount(int accountNum, String accountHolderName, String username, String password, List<Transaction> importTransactions) {
+        this(accountNum, accountHolderName, username, password);
         transactions.addAll(importTransactions);
     }
 
@@ -27,13 +39,27 @@ public class PersonalAccount implements Account {
         return accountNum;
     }
 
-    // There should be no way to change an account number after account creation
-
     @Override
     public String getAccountName() {
         return accountHolderName;
     }
 
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public void setPassword(String newPassword) {
+        this.saveHashedPassword(newPassword);
+    }
+
+    @Override
     public void setAccountName(String accountHolderName) {
         this.accountHolderName = accountHolderName;
     }
