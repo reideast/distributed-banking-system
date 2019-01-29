2e7aff52d6a0d36a39e006e22a09afbba0c53020
Andrew Reid East <andrew@andreweast.net>
2019-01-29 15:23:24 +0000

    Create simple run scripts (Windows-only) which will surely need to be made more robust later

---
+++ run-registry.ps1
@@ -0,0 +1,3 @@
+# Define CLASSPATH for rmiregistry: https://stackoverflow.com/a/23643744
+#Start-Process rmiregistry.exe "-J-Djava.class.path=target\shared.jar"
+rmiregistry.exe "-J-Djava.class.path=target\shared.jar"
---
+++ run-server.ps1
@@ -0,0 +1,2 @@
+java -cp "target\server.jar;target\shared.jar" "-Djava.security.policy=.\target\classes\server.policy" net.teamtrycatch.server.Bank
+#Start-Process -FilePath java -ArgumentList '-cp "target\server.jar;target\shared.jar" "-Djava.security.policy=.\target\classes\server.policy" net.teamtrycatch.server.Bank'
