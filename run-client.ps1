c29914521daaadb18d8c4286560cc4752f387bae
Andrew Reid East <andrew@andreweast.net>
2019-01-31 11:57:19 +0000

    Fix client run script and update README with how to develop, compile, and run

--- run-client.ps1
+++ run-client.ps1
@@ -1,7 +1,1 @@
-# Define CLASSPATH for rmiregistry: https://stackoverflow.com/a/23643744
-Start-Process rmiregistry.exe "-J-Djava.class.path=compute.jar"
-
-#java -cp ".;compute.jar" "-Djava.security.policy=.\server.policy" engine.ComputeEngine
-Start-Process -FilePath java -ArgumentList '-cp ".;compute.jar" "-Djava.security.policy=.\server.policy" engine.ComputeEngine'
-
-java -cp ".;compute.jar" "-Djava.security.policy=.\client.policy" client.ComputePi localhost 50
+java -cp "target\client.jar;target\shared.jar" "-Djava.security.policy=.\target\classes\client.policy" net.teamtrycatch.client.ATM localhost 7777
