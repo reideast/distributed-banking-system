51a2d7d73f201c60c34a793c3a8fe23efeb89785
Andrew Reid East <andrew@andreweast.net>
2019-02-03 18:45:13 +0000

    Move createMockAccounts to account datastore, taking responsiblity away from Bank class

--- src/main/java/net/teamtrycatch/server/AccountDatastoreImpl.java
+++ src/main/java/net/teamtrycatch/server/AccountDatastoreImpl.java
@@ -2,14 +2,21 @@ package net.teamtrycatch.server;
 
 import net.teamtrycatch.shared.AccountNotFoundException;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import java.util.logging.Logger;
 
 public class AccountDatastoreImpl implements AccountDatastore {
     private List<Account> accounts; // Represents the actual "database"
     private HashMap<String, Account> usernameIndex; // Represents an "index" on the database
     private HashMap<Integer, Account> accountNumberIndex; // Represents an "index" on the database
+
+    private static Logger logger = Logger.getLogger("AccountDatastoreImpl");
 
     public AccountDatastoreImpl() {
         accounts = new ArrayList<>();
@@ -51,4 +58,34 @@ public class AccountDatastoreImpl implements AccountDatastore {
             throw new AccountNotFoundException("Username + '" + username + "' was not found");
         }
     }
+
+    // TODO: JavaDoc
+    public static void createMockAccounts(AccountDatastore accounts) {
+        try {
+            DateFormat df = new SimpleDateFormat("dd MMM yyyy HH:mm");
+            PersonalAccount jack = new PersonalAccount(100, "Jack Doe", "username1", "password1");
+            jack.addTransaction(new InitialTransaction(df.parse("22 Feb 2018 16:21"), 1000));
+            jack.addTransaction(new WithdrawalTransaction(df.parse("1 Mar 2018 11:30"), 311));
+            jack.addTransaction(new DepositTransaction(df.parse("23 Mar 2018 10:00"), 1200));
+            accounts.add(jack);
+            logger.info("Added account: " + jack + " with password " + "password1");
+
+            PersonalAccount jane = new PersonalAccount(200, "Jane Doe", "username2", "password2");
+            jane.addTransaction(new InitialTransaction(df.parse("20 Mar 2016 10:30"), 2000));
+            jane.addTransaction(new DepositTransaction(df.parse("1 Apr 2016 14:12"), 1500));
+            jane.addTransaction(new WithdrawalTransaction(df.parse("2 Apr 2016 12:55"), 120));
+            jane.addTransaction(new WithdrawalTransaction(df.parse("2 Apr 2016 14:21"), 18));
+            jane.addTransaction(new WithdrawalTransaction(df.parse("14 Aug 2018 13:51"), 220));
+            jane.addTransaction(new DepositTransaction(df.parse("1 Sep 2018 14:05"), 1850));
+            accounts.add(jane);
+            logger.info("Added account: " + jane + " with password " + "password2");
+        } catch (DuplicateAccountInformationException e) {
+            logger.severe("Could not set up server! Duplicate account created: " + e.getMessage());
+            throw new RuntimeException(e); // This SHOULD crash the server process, so throw a RuntimeException
+        } catch (ParseException e) {
+            logger.severe("Could not set up server! Date could not be parsed: " + e.getMessage());
+            throw new RuntimeException(e); // This SHOULD crash the server process, so throw a RuntimeException
+        }
+    }
+
 }
--- src/main/java/net/teamtrycatch/server/Bank.java
+++ src/main/java/net/teamtrycatch/server/Bank.java
@@ -6,9 +6,6 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,36 +16,7 @@ public class Bank implements BankInterface {
 
     public Bank() throws RemoteException {
         accounts = new AccountDatastoreImpl();
-        this.createMockAccounts(); // Note: This is for the simplified application only. A real app would use a database for these
-    }
-
-    // TODO: JavaDoc
-    private void createMockAccounts() {
-        try {
-            DateFormat df = new SimpleDateFormat("dd MMM yyyy HH:mm");
-            PersonalAccount jack = new PersonalAccount(100, "Jack Doe", "username1", "password1");
-            jack.addTransaction(new InitialTransaction(df.parse("22 Feb 2018 16:21"), 1000));
-            jack.addTransaction(new WithdrawalTransaction(df.parse("1 Mar 2018 11:30"), 311));
-            jack.addTransaction(new DepositTransaction(df.parse("23 Mar 2018 10:00"), 1200));
-            accounts.add(jack);
-            logger.info("Added account: " + jack + " with password " + "password1");
-
-            PersonalAccount jane = new PersonalAccount(200, "Jane Doe", "username2", "password2");
-            jane.addTransaction(new InitialTransaction(df.parse("20 Mar 2016 10:30"), 2000));
-            jane.addTransaction(new DepositTransaction(df.parse("1 Apr 2016 14:12"), 1500));
-            jane.addTransaction(new WithdrawalTransaction(df.parse("2 Apr 2016 12:55"), 120));
-            jane.addTransaction(new WithdrawalTransaction(df.parse("2 Apr 2016 14:21"), 18));
-            jane.addTransaction(new WithdrawalTransaction(df.parse("14 Aug 2018 13:51"), 220));
-            jane.addTransaction(new DepositTransaction(df.parse("1 Sep 2018 14:05"), 1850));
-            accounts.add(jane);
-            logger.info("Added account: " + jane + " with password " + "password2");
-        } catch (DuplicateAccountInformationException e) {
-            logger.severe("Could not set up server! Duplicate account created: " + e.getMessage());
-            throw new RuntimeException(e); // This SHOULD crash the server process, so throw a RuntimeException
-        } catch (ParseException e) {
-            logger.severe("Could not set up server! Date could not be parsed: " + e.getMessage());
-            throw new RuntimeException(e); // This SHOULD crash the server process, so throw a RuntimeException
-        }
+        AccountDatastoreImpl.createMockAccounts(accounts); // Note: This is for the simplified application only. A real app would use a database for these
     }
 
     // TODO: What do I do with the "throws RemoteException". Nothing I'm doing will throw that...
@@ -103,7 +71,7 @@ public class Bank implements BankInterface {
      * Start the Bank server
      * Much of this code is derived from the Oracle Java RMI Tutorial path
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RemoteException {
         // Parse command line arguments
         int port;
         if (args.length >= 1) {
@@ -128,6 +96,7 @@ public class Bank implements BankInterface {
             // Bind compute engine to name server
             Registry registry = LocateRegistry.getRegistry();
             registry.rebind("Bank", stub);
+
             logger.info("Bank server has been launched and bound to port " + port);
 
             //DEBUG:
@@ -142,9 +111,8 @@ public class Bank implements BankInterface {
                 logger.log(Level.SEVERE, e.getMessage(), e); // Swallow exception
             }
         } catch (RemoteException e) {
-            // Swallow exception
-            logger.severe("Bank Server RemoteException!");
-            e.printStackTrace();
+            logger.severe("Server could not startup due to a remote exception" + e.getMessage());
+            throw e; // This SHOULD crash the server process, so re-throw
         }
     }
 }
