4fedae184109698baae61ccdf6970a06951e5408
Andrew Reid East <andrew@andreweast.net>
2019-02-04 12:10:45 +0000

    Implement session storage and checking

--- src/main/java/net/teamtrycatch/server/Bank.java
+++ src/main/java/net/teamtrycatch/server/Bank.java
@@ -2,23 +2,39 @@ package net.teamtrycatch.server;
 
 import net.teamtrycatch.shared.*;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.time.Duration;
+import java.time.temporal.TemporalAmount;
 import java.util.Date;
+import java.util.Random;
 import java.util.logging.Logger;
 
 public class Bank implements BankInterface {
-    AccountDatastore accounts;
     private static Logger logger = Logger.getLogger("Bank");
+
+    AccountDatastore accounts;
+
+    private Random rnd = new Random();
+    private static final TemporalAmount FIVE_MINUTES = Duration.ofMinutes(5);
 
     public Bank() throws RemoteException {
         accounts = new AccountDatastoreImpl();
     }
 
     // TODO: What do I do with the "throws RemoteException". Nothing I'm doing will throw that...
-    public long login(String username, String password) throws RemoteException, InvalidLogin {
+    public long login(String username, String password) throws RemoteException, InvalidLogin, ServerException {
         try {
             Account accountForUsername = accounts.findByUsername(username);
             if (accountForUsername.isAuth(username, password)) {
@@ -39,19 +55,73 @@ public class Bank implements BankInterface {
      * Begin a new session, generating a session ID, and saving expiration time and account number to a file
      * @param accountNum Details of account to save
      * @return Generated session ID
+     * @throws ServerException Generated if server has critical error (from IO)
      */
-    private long startNewSession(int accountNum) {
-        throw new RuntimeException("Not implemented");
+    private long startNewSession(int accountNum) throws ServerException {
+        long sessionID = Math.abs(rnd.nextLong());
+
+        try (FileWriter file = new FileWriter(sessionID + ".session")) {
+            try (PrintWriter writer = new PrintWriter(file)) {
+                // Store account number
+                writer.println(accountNum);
+
+                // Store session expiration in UNIX milliseconds
+                long expirationTime = (new Date()).toInstant().plus(FIVE_MINUTES).toEpochMilli();
+                writer.println(expirationTime);
+            }
+        } catch (IOException e) {
+            logger.severe("Could not create session file for '" + sessionID + "'");
+            throw new ServerException("Could not create new session on server");
+        }
+
+        return sessionID;
     }
 
     /**
      * Validate that this session currently exists, is valid for this time range, and is for this account number
-     * @param sessionID Session file to locate
+     * @param sessionID  Session file to locate
      * @param accountNum Number which should be inside that session file
      * @throws InvalidSession Error if session does not exist or if account number is not for this session
+     * @throws ServerException Error if server has critical error (from IO)
      */
-    private void verifySession(long sessionID, int accountNum) throws InvalidSession {
-        throw new RuntimeException("Not implemented");
+    private void verifySession(long sessionID, int accountNum) throws InvalidSession, ServerException {
+        try (FileReader file = new FileReader(sessionID + ".session")) {
+            int accountNumLine;
+            long expirationTimeLine;
+
+            try (BufferedReader reader = new BufferedReader(file)) {
+                try {
+                    accountNumLine = Integer.parseInt(reader.readLine());
+                    expirationTimeLine = Long.parseLong(reader.readLine());
+                } catch (NumberFormatException e) { // Thrown both if not a valid number or if readLine failed and returned null
+                    logger.severe("Malformed session file: " + sessionID);
+                    throw new InvalidSession("Session invalid");
+                }
+            } catch (IOException e) {
+                logger.severe("Could not read from session file, IO error" + sessionID);
+                throw new ServerException("Could not read session on server");
+            }
+
+            // Validate account number in file
+            if (accountNumLine != accountNum) {
+                logger.warning("User attempted to use session " + sessionID + " (stored account number " + accountNumLine + "), but provided incorrect account number: " + accountNum);
+                invalidateSession(sessionID);
+                throw new InvalidSession("Session ID is not valid for account number");
+            }
+
+            // Validate current time is not after the stored expiration time (in UNIX ms)
+            if ((new Date()).after(new Date(expirationTimeLine))) {
+                logger.warning("User session has expired: " + sessionID);
+                invalidateSession(sessionID);
+                throw new InvalidSession("Session has expired");
+            }
+        } catch (FileNotFoundException e) {
+            logger.severe("Client attempted to utilise an invalid session ID: " + sessionID);
+            throw new InvalidSession("Session ID not found");
+        } catch (IOException e) {
+            logger.severe("Could not open session file, IO error: " + sessionID);
+            throw new ServerException("Could not read session on server");
+        }
     }
 
     /**
@@ -59,10 +129,17 @@ public class Bank implements BankInterface {
      * @param sessionID Session file to find. May or may not exist on disk
      */
     private void invalidateSession(long sessionID) {
-        throw new RuntimeException("Not implemented");
+        try {
+            Files.deleteIfExists((new File(sessionID + ".session")).toPath());
+            logger.info("Removing session: " + sessionID);
+        } catch (NoSuchFileException e) {
+            logger.warning("Removing session, file has already been removed: " + sessionID);
+        } catch (IOException e) {
+            logger.severe("Cannot remove session, file IO error: " + sessionID);
+        }
     }
 
-    public void deposit(int accountNum, int amount, long sessionID) throws RemoteException, InvalidSession, AccountNotFoundException {
+    public void deposit(int accountNum, int amount, long sessionID) throws RemoteException, InvalidSession, AccountNotFoundException, ServerException {
         if (amount < 0) {
             throw new IllegalArgumentException("Amount must not be negative");
         }
@@ -71,7 +148,7 @@ public class Bank implements BankInterface {
         account.addTransaction(new DepositTransaction(new Date(), amount));
     }
 
-    public void withdraw(int accountNum, int amount, long sessionID) throws RemoteException, InvalidSession, AccountNotFoundException {
+    public void withdraw(int accountNum, int amount, long sessionID) throws RemoteException, InvalidSession, AccountNotFoundException, ServerException {
         if (amount < 0) {
             throw new IllegalArgumentException("Amount must not be negative");
         }
@@ -80,13 +157,13 @@ public class Bank implements BankInterface {
         account.addTransaction(new WithdrawalTransaction(new Date(), amount));
     }
 
-    public int inquiry(int accountNum, long sessionID) throws RemoteException, InvalidSession, AccountNotFoundException {
+    public int inquiry(int accountNum, long sessionID) throws RemoteException, InvalidSession, AccountNotFoundException, ServerException {
         Account account = accounts.findByAccountNum(accountNum);
         verifySession(sessionID, accountNum);
         return account.getBalance();
     }
 
-    public Statement getStatement(int accountNum, Date from, Date to, long sessionID) throws RemoteException, InvalidSession, AccountNotFoundException {
+    public Statement getStatement(int accountNum, Date from, Date to, long sessionID) throws RemoteException, InvalidSession, AccountNotFoundException, ServerException {
         if (to.before(from)) {
             throw new IllegalArgumentException("From date must be chronologically before To date");
         }
