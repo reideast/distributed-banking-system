751db2c4f7cb9ef2261c9d7a6fb92a125ab427e2
Andrew Reid East <andrew@andreweast.net>
2019-02-05 19:57:07 +0000

    Fix windows scripts...mainly spelling 'inquiry' correctly >_<

--- run-client.ps1
+++ run-client.ps1
@@ -1,5 +1,5 @@
 # The client should have its own files and shared interfaces.
 # It gets those interfaces which are implemented on the server from the codebase feature
 # This powershell script supports adding in arguments from the prompt
-#     So, run this scipt as: `.\run-client.ps1 login user pass`, `.\run-client.ps1 inquery 100`, etc.
+#     So, run this script as: `.\run-client.ps1 login user pass`, `.\run-client.ps1 inquiry 100`, etc.
 java -cp "target\client.jar;target\interfaces.jar;target\shared-from-client.jar" "-Djava.rmi.server.codebase=file:target\shared-from-server.jar" "-Djava.security.policy=.\target\classes\client.policy" net.teamtrycatch.client.ATM localhost 7777 $args
--- run-registry.ps1
+++ run-registry.ps1
@@ -1,3 +1,4 @@
 # The registry should have the shared remote interfaces in its CLASSPATH, but no other compiled files
 #     How to Define CLASSPATH for rmiregistry: https://stackoverflow.com/a/23643744
 rmiregistry.exe "-J-Djava.class.path=target\interfaces.jar"
+Write-Host "RMI Registry has been started"
