4987f83de207f3ef8d52d755f07ac3ff005ee94b
Andrew Reid East <andrew@andreweast.net>
2019-02-08 20:42:48 +0000

    Fix linux scripts

--- run-registry.sh
+++ run-registry.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # The registry should have the shared remote interfaces in its CLASSPATH, but no other compiled files
 #     How to Define CLASSPATH for rmiregistry: https://stackoverflow.com/a/23643744
-rmiregistry.exe -J-Djava.class.path=./target/interfaces.jar &
+rmiregistry.exe 7777 -J-Djava.class.path=./target/interfaces.jar &
 echo RMI Registry has been started
